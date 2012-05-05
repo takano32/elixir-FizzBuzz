add4990185c1eedfaee11735e66a5f8109a49672
TAKANO Mitsuhiro <takano32@gmail.com>
2012-05-05 20:18:01 +0900

    impl Fizz Buzz in Elixir

--- fizzbuzz.ex
+++ fizzbuzz.ex
@@ -1,8 +1,26 @@
 #!/usr/bin/env elixir
 
+fb = fn(n) do
+	loop n do
+	match: 0
+		0
+	match: x
+		recur(x - 1)
+		case { rem(x, 5) == 0, rem(x, 3) == 0} do
+		match: {true, true}
+			IO.puts "FizzBuzz"
+		match: {true, false}
+			IO.puts "Buzz"
+		match: {false, true}
+			IO.puts "Fizz"
+		else:
+			IO.puts x
+		end
+	end
+end
 
 
-
+fb.(100)
 
 
 
